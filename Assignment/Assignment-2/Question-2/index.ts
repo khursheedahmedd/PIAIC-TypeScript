152a0fcb54268b28b4eec7d5afa23b6262b5525f
Hajra-wrec76 <122016973+Hajra-wrec76@users.noreply.github.com>
2024-07-20 11:00:18 +0500

    Update index.ts

--- Assignment/Assignment-2/Question-2/index.ts
+++ Assignment/Assignment-2/Question-2/index.ts
@@ -1,3 +1,3 @@
 const currentDate: Date = new Date();
 const formattedDate: string = `${currentDate.getDate()}-${currentDate.toLocaleString('default', { month: 'long' })}-${currentDate.getFullYear()}`;
-console.log(`Today Date : ${formattedDate}`);
+console.log(`Today Date : ${formattedDate}`); 
