b33e9fb7f1114adeba2dd30e5855185da0f440d0
khursheedahmedd <118465990+khursheedahmedd@users.noreply.github.com>
2024-08-12 23:28:53 +0500

    ..

--- tailwindcss/src/app/(pages)/(auth)/layout.tsx
+++ tailwindcss/src/app/(pages)/(auth)/layout.tsx
@@ -4,10 +4,6 @@ import { Metadata } from "next";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export const metadate: Metadata = {
-  title: "Auth",
-};
-
 const navLinks = [
   { name: "Register", href: "/register" },
   { name: "Login", href: "/login" },
